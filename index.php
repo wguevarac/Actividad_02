b7bb65146c8dab8459c6b1331104f71b14433dd6
Wilmar Guevara <wguevarac@unadvirtual.edu.co>
2016-09-20 07:11:20 -0500

    inicio barner
    incrusta imagen y se acomoda plantilla para inicios del css3

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="es" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta content="Wilmar Guevara" name="author"/>
+	<meta content="Pagina Ova 1" name="description"/>
+	<meta content="Unad, Ova, taller" name="keyword"/>
+	<title>Ova 01</title>
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
+
+<header id="main-header">
+<div id="main-logo">
+               <div class="imagen"> <img src="img/logo.png"  width="30%"></div>
+				<a href="index.php">OBJETO VIRTUAl DE APRENDIZAJE PARA LA INICIACION EN PAGINAS WEB</a>
+</div>
+</header>
+
+<nav>
+
+				<ul id="main-menu">
+							<li><a href="formaleta.php">Formaleta Metalica</a></li>
+							<li><a href="accesorios.php">Accesorios</a></li>
+							<li><a href="grupo-3.php">Apuntalamiento</a></li>							
+                            <li><a href="grupo-4.php">Consumibles</a></li>
+						    <li><a href="grupo-5.php">Mantenimiento</a></li>
+						
+				  </li>
+                </ul>
+
+</nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--<div id="contenido">
+
+<header>
+<div id="cabecera"
+<hgrupo>
+	<h1>OBJETO VIRTUAl DE APRENDIZAJE PARA LA INICIACION EN PAGINAS WEB</h1>
+	<H2>OVa de aprendizaje Activida Dos</H2>
+</hgrupo>
+</div>	
+</header>
+
+<nav>
+<div class="menu">
+	<ul>
+		<li>
+			<a href="">Inicio</a>
+			<a href="">Lecturas</a>
+			<a href="">Multimedia</a>
+			<a href="">Actividades</a>
+			<a href="">Autores</a>
+		</li>
+	</ul>
+</div>	
+</nav>
+
+<section>
+	<article></article>
+</section>
+
+<aside></aside>
+
+<footer>
+<p>Propiedad Alumno Wilmar Guevara 19-09-16</p>
+</footer>
+
+
+
+</div>-->
+</body>
+
+
+<script src="js/modernizr-custom.js"></script>
+<script src="js/prefixfree.min.js"></script>
+<script src="js/jquery-3.1.0.min.js"></script>
+
+</html>
